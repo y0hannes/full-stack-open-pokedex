f3130ce0b10a34c1f583dea700bebd8793262352
y0hannes <yohanneset3@gmail.com>
2026-02-17 20:53:21 +0300

    feat: Implement Playwright end-to-end tests for front page and Pokemon navigation.

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,19 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('');
+    await expect(page.getByText('ivysaur')).toBeVisible();
+    await expect(
+      page.getByText(
+        'Pokémon and Pokémon character names are trademarks of Nintendo.',
+      ),
+    ).toBeVisible();
+  });
+
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('');
+    await page.click('text=ivysaur');
+    await expect(page.getByText('chlorophyll')).toBeVisible();
+  });
+});
---
+++ playwright.config.js
@@ -0,0 +1,25 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './e2e-tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:5000',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'npm run start-prod',
+    url: 'http://localhost:5000',
+    reuseExistingServer: !process.env.CI,
+  },
+});
